47f29f85101243512ee6aa0f7a27139da3f67c9c
thearyadev <personal@aryankothari.dev>
2023-03-12 14:39:29 -0400

    filter placeholder file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dotenv.load_dotenv()
 if __name__ == '__main__':
     while True:
         time.sleep(int(os.getenv("INTERVAL")))
-        files = os.listdir("./data")
+        files = [f for f in os.listdir("./data") if ".txt" not in f]
         random.shuffle(files)
         with open("./data/" + files[0], "rb") as image_file:
             discord.set_profile_picture(auth_token=os.getenv("TOKEN"), image=image_file.read())
